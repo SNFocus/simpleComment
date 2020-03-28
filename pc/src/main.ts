0e4862f0c7698932154ee50da7d0a9515d133b78
soning <deng_suning@163.com>
2020-03-28 10:15:14 +0800

    build(store/ and main.ts): add Vuex dependience

--- pc/src/main.ts
+++ pc/src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store/store'
 import { Button, Layout, Menu, Icon } from 'ant-design-vue'
 
 Vue.use(Button)
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ pc/src/store/store.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store: any = new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {}
+})
+
+export default store
